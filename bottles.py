b614995c587272fb8d8c57ee3a5d96ee8613a3f9
tylerReis <tyler.reis.j@outlook.com>
2024-02-01 15:17:22 -0500

    first solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,17 @@
-def bottle_song():
-	# write your code here!
-	pass
+from os import lseek
 
-bottle_song()
 
-####kldjsfalkfdajalskdfjdfls
+def bottle_song(num):
+	while num > 1:
+		print(f"{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num - 1} bottles of beer on the wall.")
+		num =  num - 1	
+	if num == 1:
+		print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+		num = num - 1
+	return "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall."
+		
+
+
+
+print(bottle_song(99))
+
